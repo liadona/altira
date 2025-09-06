c9449f2c3f8149c3c0d0781f9391c85a06feb422
liadona <sabat.adons@gmail.com>
2025-09-06 07:57:07 +0700

    fix: package.json scripts & db.js for Railway

--- server/db.js
+++ server/db.js
@@ -1,20 +1,9 @@
 import pkg from "pg";
 const { Pool } = pkg;
 
-// sesuaikan dengan PostgreSQL kamu
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "myfarmdb",
-  password: "Sabat.adons^10", // ganti sesuai password PostgreSQL kamu
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
 });
-
-try {
-  await pool.connect();
-  console.log("Connected to PostgreSQL database successfully");
-} catch (err) {
-  console.error("Database connection error:", err);
-}
 
 export default pool;
