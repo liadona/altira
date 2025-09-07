5df9de487adcdebc0915d9c8a6f74b95a4802064
liadona <sabat.adons@gmail.com>
2025-09-07 09:29:35 +0700

    coba

--- server/seed.js
+++ server/seed.js
@@ -7,7 +7,7 @@ async function seed() {
       CREATE TABLE IF NOT EXISTS users (
         id SERIAL PRIMARY KEY,
         username VARCHAR(100) UNIQUE NOT NULL,
-        password VARCHAR(255) NOT NULL,
+        password STRING(255) NOT NULL,
         role VARCHAR(20) DEFAULT 'admin'
       );
     `);
